e3c310c0153999d0a45c038395faf7f943ee6cd9
BillNg2801 <mailtobin05@gmail.com>
2026-01-05 03:47:20 -0800

    Revert to Root Directory approach: move vercel.json to backend folder

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// Vercel serverless function entry point
-// This imports the Express app from backend/server.js
-const path = require('path');
-
-// Use absolute path to ensure it works in Vercel
-const app = require(path.join(__dirname, '../backend/server.js'));
-
-module.exports = app;
-
